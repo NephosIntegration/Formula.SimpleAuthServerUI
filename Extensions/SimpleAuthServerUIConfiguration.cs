98dac41545a0f46825fd2ba4c95f48e9ffe4a79a
Joshua Williams <joshua.p.williams@gmail.com>
2019-12-14 23:46:18 -0600

    Finished Authentication

--- Extensions/SimpleAuthServerUIConfiguration.cs
+++ Extensions/SimpleAuthServerUIConfiguration.cs
@@ -7,23 +7,22 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Formula.SimpleAuthServerUI
 {
     public static class SimpleAuthServerUIConfiguration
     {
-        public static IServiceCollection AddSimpleAuthServerUI(this IServiceCollection services)
+        public static IServiceCollection AddSimpleAuthServerUI(this IServiceCollection services, AuthenticationBuilder authenticationBuilder)
         {
+            // Enable all the log in / log out / consent etc.. Views
             services.AddControllersWithViews();
             
+            // We will change the claim map, so don't use the default
             JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
 
-            
-            var builder = services.AddAuthentication(options => {
-                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            })
-            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+            // The views will handle the authentication via cookies
+            authenticationBuilder.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
 
             return services;
         }
